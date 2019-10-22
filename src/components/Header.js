5851d2daa7a863b2bdc435993401162baee97b54
Nelson Dantis <dantisnelson@gmail.com>
2019-10-21 19:51:05 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,42 @@
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import Image from "./image";
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <container>
+    <div
+      style={{
+        maxWidth: `100px`,
+        marginBottom: `1.45rem`,
+        marginTop: `1.45rem`,
+        marginLeft: `1.45rem`,
+      }}
+    >
+      <Link to="/">
+        <Image />
+      </Link>
     </div>
-  </header>
+    <header-container>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#home"> Home </a>
+      </div>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#about"> About Us </a>
+      </div>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#team"> Our Team </a>
+      </div>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#partners"> Our Partners </a>
+      </div>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#events"> Events </a>
+      </div>
+      <div style={{ marginTop: `4rem` }}>
+        <a href="#contact"> Contact </a>
+      </div>
+    </header-container>
+  </container>
 );
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: ``,
-};
 
 export default Header;
